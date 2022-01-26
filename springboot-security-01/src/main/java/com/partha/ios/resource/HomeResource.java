3b621e5dc36b741d1a56e8b82b6187c3cae518cf
prathap <prathapgowda09@gmail.com>
2022-01-26 15:22:55 +0530

    springboot authorization added

--- springboot-security-01/src/main/java/com/partha/ios/resource/HomeResource.java
+++ springboot-security-01/src/main/java/com/partha/ios/resource/HomeResource.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeResource {
 
     @GetMapping("/")
-    public String welcomeHome(){
-        return "Home Page!!";
+    public String home(){
+        return ("<h1>Welcome..!!</h1>");
     }
+
+    @GetMapping("/user")
+    public String user(){
+        return ("<h1>Welcome User!!</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>Welcome Admin!!</h1>");
+    }
+
+
 }
