7c8b4db19bfed75d65b9d982a72b93f58fb351d2
prathap <prathapgowda09@gmail.com>
2022-01-26 16:43:32 +0530

    setup JDBC authentication with Spring Security

---
+++ springboot-security-jdbc/src/main/java/com/partha/ios/SpringbootSecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.partha.ios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityJdbcApplication.class, args);
+	}
+
+}
